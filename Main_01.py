e3161a69f826a9d8f9ac479f29b79201b871df1e
VisionSystem31 <vsistema31@gmail.com>
2023-11-11 01:58:36 +0100

    Vector

--- Main_01.py
+++ Main_01.py
@@ -8,42 +8,43 @@ from ultralytics import YOLO
 import subprocess
 
 def visualizar():
-    global inicio, cap, frame, model, class_names, texto1, texto3, BoxShadow_id, counter
+    global inicio, cap, frame, model, class_names, texto1, texto3, BoxShadow_id, counter, vector
 
     if inicio == 1:
-        model = YOLO("Dimples_N01.pt")
-        cap = cv2.VideoCapture("Dimples_Video_02.mp4")
+        model = YOLO("Dimples_S01.pt")
+        cap = cv2.VideoCapture(0)
         # cap.set(4, 480) #Alto
         # cap.set(3, 640) #Ancho
         class_names = ["Dimple"]
         inicio = 0
         counter = 1
+        vector = []
     else:
         pantalla.delete(frame)
         pantalla.delete(texto1)
         pantalla.delete(texto3)
         pantalla.delete(BoxShadow_id)
 
-    if counter < 8:
-        BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
-        Close_Button.configure(bg = "#FFFFFF")
-        pantalla.configure(bg = "#FFFFFF")
-        texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="black")
-        texto3 = pantalla.create_text(500, 640, text=f"Waiting for the Dimple", font=("Helvetica", 30, "bold"), fill="black")
+    # if counter < 8:
+    #     BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
+    #     Close_Button.configure(bg = "#FFFFFF")
+    #     pantalla.configure(bg = "#FFFFFF")
+    #     texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="black")
+    #     texto3 = pantalla.create_text(500, 640, text=f"Waiting for the Dimple", font=("Helvetica", 30, "bold"), fill="black")
 
-    elif counter < 10:
-        BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
-        Close_Button.configure(bg = "#008000")
-        pantalla.configure(bg = "#008000")
-        texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="white")
-        texto3 = pantalla.create_text(500, 640, text=f"Dimple Found", font=("Helvetica", 30, "bold"), fill="white")
+    # elif counter < 10:
+    #     BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
+    #     Close_Button.configure(bg = "#008000")
+    #     pantalla.configure(bg = "#008000")
+    #     texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="white")
+    #     texto3 = pantalla.create_text(500, 640, text=f"Dimple Found", font=("Helvetica", 30, "bold"), fill="white")
 
-    else:
-        Close_Button.configure(bg = "#FF0035")
-        pantalla.configure(bg = "#FF0035")
-        BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
-        texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="white")
-        texto3 = pantalla.create_text(500, 640, text=f"Waiting for detection...", font=("Helvetica", 30, "bold"), fill="white")
+    # else:
+    #     Close_Button.configure(bg = "#FF0035")
+    #     pantalla.configure(bg = "#FF0035")
+    #     BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
+    #     texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="white")
+    #     texto3 = pantalla.create_text(500, 640, text=f"Waiting for detection...", font=("Helvetica", 30, "bold"), fill="white")
 
     if cap is not None:
         ret, frame = cap.read()
@@ -63,17 +64,22 @@ def visualizar():
                             if class_id == 0: 
                                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 4)
                                 cv2.putText(frame, f"{class_names[0]}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2,)
-                                # BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
-                                # Close_Button.configure(bg = "#008000")
-                                # pantalla.configure(bg = "#008000")
-                                # texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="white")
-                                # texto3 = pantalla.create_text(500, 640, text=f"Dimple Found", font=("Helvetica", 30, "bold"), fill="white")
-                    # else:
-                    #     BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
-                    #     Close_Button.configure(bg = "#FFFFFF")
-                    #     pantalla.configure(bg = "#FFFFFF")
-                    #     texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="black")
-                    #     texto3 = pantalla.create_text(500, 640, text=f"Waiting for the Dimple", font=("Helvetica", 30, "bold"), fill="black")
+                                vector.append(class_id)
+                    else:
+                        vector = [] 
+            if len(vector) == 5:
+                vector = []
+                BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
+                Close_Button.configure(bg = "#008000")
+                pantalla.configure(bg = "#008000")
+                texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="white")
+                texto3 = pantalla.create_text(500, 640, text=f"Dimple Found", font=("Helvetica", 30, "bold"), fill="white")
+            else:
+                BoxShadow_id = pantalla.create_image(155, 75, anchor=tk.NW, image=BoxShadow)
+                Close_Button.configure(bg = "#FFFFFF")
+                pantalla.configure(bg = "#FFFFFF")
+                texto1 = pantalla.create_text(500, 55, text="Dimples Vision System", font=("Helvetica", 30, "bold"), fill="black")
+                texto3 = pantalla.create_text(500, 640, text=f"Waiting for the Dimple", font=("Helvetica", 30, "bold"), fill="black")
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame = imutils.resize(frame, width=640, height=480)
